e744423a5c54a4ffa5a9cb6240cd9a07d0ce5bb1
ryomario <ardamomo3@gmail.com>
2022-09-03 11:19:28 +0700

    dev: * - articles - show the token owner

--- controllers/ArticlesController.js
+++ controllers/ArticlesController.js
@@ -9,7 +9,12 @@ const ArticelsController = Router()
  * List Articles
  */
 ArticelsController.get('/', userSession, async (req, res, next) => {
+    // console.log('user',req.user)
+    // req.user is created in userSession function
     const list = await m$article.listArticles(req.query)
+
+    // Include user who access
+    list.accessed_by = req.user
 
     response.sendResponse(res, list)
 })
@@ -21,6 +26,9 @@ ArticelsController.get('/detail', userSession, async (req, res, next) => {
     // http://url-api/detail?id=1
     const detail = await m$article.detailArticle(req.query.id)
 
+    // Include user who access
+    detail.accessed_by = req.user
+
     response.sendResponse(res, detail)
 })
 
@@ -30,7 +38,14 @@ ArticelsController.get('/detail', userSession, async (req, res, next) => {
  * @param {string} summary
  */
 ArticelsController.post('/', userSession, async (req, res, next) => {
+
+    // Include user id who created to the body
+    req.body.user_id = req.user.id
+
     const add = await m$article.addArticle(req.body)
+
+    // Include user who created
+    add.created_by = req.user
 
     response.sendResponse(res, add)
 })
